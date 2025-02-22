e9cbf6cf1c1aa12665f1e614ba7b01a5d4bb883e
Sid Suri <176190714+SidxSuri@users.noreply.github.com>
2025-02-22 12:11:33 -0500

    Update google_analytics.js

--- google_analytics.js
+++ google_analytics.js
@@ -1,5 +1,20 @@
-// Google Analytics Script
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-HFMLHSXTWF', {'anonymize_ip': true});
+<!-- Google Analytics Script -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HFMLHSXTWF"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-HFMLHSXTWF', {'anonymize_ip': true});
+    
+    // Social media click tracking
+    document.querySelectorAll('.tile a').forEach(link => {
+        link.addEventListener('click', () => {
+            gtag('event', 'social_click', {
+                'event_category': 'social',
+                'event_label': link.href
+            });
+        });
+    });
+</script>
