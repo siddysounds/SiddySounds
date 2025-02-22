a912595bdbdb080a8163757e133b2462161aa53e
Sid Suri <176190714+SidxSuri@users.noreply.github.com>
2025-02-22 02:04:47 -0500

    Create google_analytics.js

---
+++ google_analytics.js
@@ -0,0 +1,5 @@
+// Google Analytics Script
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-HFMLHSXTWF', {'anonymize_ip': true});
