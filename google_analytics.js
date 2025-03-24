ee79ccf274b167fe420a4cc01dc1c3a5dec393e5
Sid Suri <sid.suri@icloud.com>
2025-03-23 20:09:01 -0400

    Update google_analytics.js

--- google_analytics.js
+++ google_analytics.js
@@ -1,20 +1,9 @@
-<!-- Google Analytics Script -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-HFMLHSXTWF"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZXPLTQ4NYQ"></script>
 <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', 'G-HFMLHSXTWF', {'anonymize_ip': true});
-    
-    // Social media click tracking
-    document.querySelectorAll('.tile a').forEach(link => {
-        link.addEventListener('click', () => {
-            gtag('event', 'social_click', {
-                'event_category': 'social',
-                'event_label': link.href
-            });
-        });
-    });
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZXPLTQ4NYQ');
 </script>
