59de2fc49d0d8a9051234f522053087ef91c113e
Sid Suri <sid.suri@icloud.com>
2025-03-26 19:36:54 -0400

    submit
    eeee

--- firebase_script.js
+++ firebase_script.js
@@ -1,5 +1,5 @@
-// Firebase configuration
-const firebaseConfig = {
+// Initialize Firebase
+const app = firebase.initializeApp({
   apiKey: "AIzaSyCxEbW9TRG4SrjPyoOmsgezjvq0HkADj04",
   authDomain: "siddy-sounds.firebaseapp.com",
   projectId: "siddy-sounds",
@@ -7,10 +7,9 @@ const firebaseConfig = {
   messagingSenderId: "710437474568",
   appId: "1:710437474568:web:35d3af84205338b933dcbd",
   measurementId: "G-NSP8CP2Q7Y"
-};
+});
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+// Firestore Database
 const db = firebase.firestore();
 
-console.log(firebase.apps.length ? "Loaded" : "Not loaded");
+console.log(firebase.apps.length ? "Firebase Loaded" : "Firebase Not Loaded");
