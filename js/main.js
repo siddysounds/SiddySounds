14a4f954c8215959fb2b0c05a5e91a43b08eaeb6
Sid Suri <sid.suri@icloud.com>
2025-03-26 20:57:23 -0400

    Final

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-/*  ---------------------------------------------------
-  Template Name: DJoz
-  Description:  DJoz Music HTML Template
-  Author: Colorlib
-  Author URI: https://colorlib.com
-  Version: 1.0
-  Created: Colorlib
----------------------------------------------------------  */
+
 
 'use strict';
 
